1f502dc5cf284beb4cb2fd20f133df4635f5123e
Lim, gyuseong <100032171+gyural@users.noreply.github.com>
2024-07-13 14:56:56 +0900

    initial-commit

---
+++ meeting_summarizer/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Meeting(models.Model):
+    title = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    transcript = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class Recording(models.Model):
+    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, related_name='recordings')
+    audio_file = models.FileField(upload_to='recordings/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Recording of {self.meeting.title}"
